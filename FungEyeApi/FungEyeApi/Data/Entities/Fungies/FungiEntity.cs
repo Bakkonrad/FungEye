ed577a54575ede681a60ea70776504b3805039c4
Konrad Bakowski <konri0908@gmail.com>
2024-10-26 17:19:23 +0200

    fungiEntity, naprawa metody predict

---
+++ FungEyeApi/FungEyeApi/Data/Entities/Fungies/FungiEntity.cs
@@ -0,0 +1,20 @@
+﻿using FungEyeApi.Data.Entities.Fungies;
+
+namespace FungEyeApi.Data.Entities
+{
+    public class FungiEntity
+    {
+        public FungiEntity() { }
+
+        public int Id { get; set; }
+        public required string LatinName { get; set; }
+        public string? PolishName { get; set; }
+        public string? Description { get; set; }
+        public string? Edibility { get; set; }
+        public string? Toxicity { get; set; }
+        public string? Habitat { get; set; }
+
+        public ICollection<FungiImageEntity> Images { get; set; }
+        public ICollection<UserFungiCollectionEntity> UserCollections { get; set; }
+    }
+}

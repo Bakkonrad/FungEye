8a156998bdb628bced323af7dca279e2a2484fec
budaala <budaala4@gmail.com>
2024-12-29 17:21:01 +0100

    added installation guide for PWA

--- FungEyeApp/src/main.js
+++ FungEyeApp/src/main.js
@@ -12,12 +12,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faMagnifyingGlass, faTrash, faBan, faPen, faList, faArrowUp, faGear, faRightFromBracket, faLeftLong, faRightToBracket, faCircleUser, faPlus, faUndo, faAnglesRight, faThumbsUp, faThumbsDown, faComment, faFlag, faBookmark, faBookAtlas, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faTrash, faBan, faPen, faList, faArrowUp, faGear, faRightFromBracket, faLeftLong, faRightToBracket, faCircleUser, faPlus, faUndo, faAnglesRight, faThumbsUp, faThumbsDown, faComment, faFlag, faBookmark, faBookAtlas, faEye, faEllipsisVertical, faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons'
+
+import { faAndroid, faApple} from '@fortawesome/free-brands-svg-icons'
 
 import { faBookmark as faBookmarkR} from '@fortawesome/free-regular-svg-icons'
 
 /* add icons to the library */
-library.add(faMagnifyingGlass, faTrash, faBan, faPen, faList, faArrowUp, faGear, faRightFromBracket, faLeftLong, faRightToBracket, faCircleUser, faPlus, faUndo, faAnglesRight, faThumbsUp, faThumbsDown, faComment, faFlag, faBookmark, faBookmarkR, faBookAtlas, faEye)
+library.add(faMagnifyingGlass, faTrash, faBan, faPen, faList, faArrowUp, faGear, faRightFromBracket, faLeftLong, faRightToBracket, faCircleUser, faPlus, faUndo, faAnglesRight, faThumbsUp, faThumbsDown, faComment, faFlag, faBookmark, faBookmarkR, faBookAtlas, faEye, faAndroid, faApple, faEllipsisVertical, faArrowUpFromBracket)
 
 
 const app = createApp(App)
